9e078127ba7c707bab19f6cb1cb3af233a12857a
DRainEm0 <96368452+DRainEm0@users.noreply.github.com>
2023-04-25 01:43:25 +0300

    Add files via upload

---
+++ BackendApi/Contracts/Account/GetUserResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace BackendApi.Contracts.Account
+{
+    public class GetUserResponse
+    {
+        public int UserID { get; set; }
+        public string Login { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string Photo { get; set; } = null!;
+        public int Phone { get; set; }
+    }
+}
