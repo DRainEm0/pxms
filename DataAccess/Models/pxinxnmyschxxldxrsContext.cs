4de7e4d3c387f84c5f4336802d758f84feaeed88
DRainEm0 <96368452+DRainEm0@users.noreply.github.com>
2023-04-25 01:45:19 +0300

    Add files via upload

---
+++ DataAccess/Models/pxinxnmyschxxldxrsContext.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DataAccess.Models
+{
+    public partial class pxinxnmyschxxldxrsContext : DbContext
+    {
+        public pxinxnmyschxxldxrsContext()
+        {
+        }
+
+        public pxinxnmyschxxldxrsContext(DbContextOptions<pxinxnmyschxxldxrsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; } = null!;
+        public virtual DbSet<Basket> Baskets { get; set; } = null!;
+        public virtual DbSet<Category> Categories { get; set; } = null!;
+        public virtual DbSet<Delivery> Deliveries { get; set; } = null!;
+        public virtual DbSet<Item> Items { get; set; } = null!;
+        public virtual DbSet<OrderItem> OrderItems { get; set; } = null!;
+        public virtual DbSet<Review> Reviews { get; set; } = null!;
+
+       
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>(entity =>
+            {
+                entity.HasKey(e => e.UserId)
+                    .HasName("PK__Account__1788CCAC01FFF667");
+
+                entity.ToTable("Account");
+
+                entity.HasIndex(e => e.Login, "UQ__Account__5E55825BA519871A")
+                    .IsUnique();
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.Login).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+
+                entity.Property(e => e.Password).HasMaxLength(50);
+
+                entity.Property(e => e.Photo).HasMaxLength(100);
+
+                entity.Property(e => e.Surname).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Basket>(entity =>
+            {
+                entity.HasKey(e => e.DealId)
+                    .HasName("PK__Basket__E5B28166DD84923A");
+
+                entity.ToTable("Basket");
+
+                entity.Property(e => e.ItemId).HasColumnName("ItemID");
+
+                entity.Property(e => e.Status).HasMaxLength(50);
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.HasOne(d => d.Item)
+                    .WithMany(p => p.Baskets)
+                    .HasForeignKey(d => d.ItemId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Basket__ItemID__2F10007B");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Baskets)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Basket__UserID__300424B4");
+            });
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.ToTable("Category");
+
+                entity.Property(e => e.CategoryId).ValueGeneratedNever();
+
+                entity.Property(e => e.CategoryName).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Delivery>(entity =>
+            {
+                entity.ToTable("Delivery");
+
+                entity.Property(e => e.DeliveryId).HasColumnName("DeliveryID");
+
+                entity.Property(e => e.Apartment).HasMaxLength(50);
+
+                entity.Property(e => e.City).HasMaxLength(50);
+
+                entity.Property(e => e.CityIndex).HasMaxLength(50);
+
+                entity.Property(e => e.Country).HasMaxLength(50);
+
+                entity.Property(e => e.Home).HasMaxLength(50);
+
+                entity.Property(e => e.Street).HasMaxLength(50);
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Deliveries)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Delivery__Apartm__2C3393D0");
+            });
+
+            modelBuilder.Entity<Item>(entity =>
+            {
+                entity.ToTable("Item");
+
+                entity.Property(e => e.ItemId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("ItemID");
+
+                entity.Property(e => e.Color).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+
+                entity.Property(e => e.Photo).HasMaxLength(50);
+
+                entity.Property(e => e.Size).HasMaxLength(50);
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Items)
+                    .HasForeignKey(d => d.CategoryId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Item__CategoryId__29572725");
+            });
+
+            modelBuilder.Entity<OrderItem>(entity =>
+            {
+                entity.HasKey(e => e.OrderId)
+                    .HasName("PK__OrderIte__C3905BAFE9E8EE37");
+
+                entity.ToTable("OrderItem");
+
+                entity.Property(e => e.OrderId).HasColumnName("OrderID");
+
+                entity.Property(e => e.DateOfCreate).HasColumnType("datetime");
+
+                entity.Property(e => e.Deadline).HasColumnType("datetime");
+
+                entity.Property(e => e.DeliveryStatus).HasMaxLength(50);
+
+                entity.Property(e => e.ItemId).HasColumnName("ItemID");
+
+                entity.HasOne(d => d.Item)
+                    .WithMany(p => p.OrderItems)
+                    .HasForeignKey(d => d.ItemId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__OrderItem__ItemI__33D4B598");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.OrderItems)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__OrderItem__UserI__32E0915F");
+            });
+
+            modelBuilder.Entity<Review>(entity =>
+            {
+                entity.ToTable("Review");
+
+                entity.Property(e => e.Opinion).HasMaxLength(1000);
+
+                entity.Property(e => e.Photo).HasMaxLength(100);
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.HasOne(d => d.Item)
+                    .WithMany(p => p.Reviews)
+                    .HasForeignKey(d => d.ItemId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Review__ItemId__37A5467C");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Reviews)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Review__Photo__36B12243");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
